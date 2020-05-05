3b21ffab18279e4e72c67a010b629157d9740ab7
Youssef <theyous@gmail.com>
2020-05-05 18:06:41 -0500

    add html

--- scrape.py
+++ scrape.py
@@ -15,3 +15,23 @@ weatherText = weatherHtmlData.get_text()
 allergyImage = urllib.request.urlretrieve("http://cdn.tegna-media.com/kvue/weather/allergy16x9.jpg", "allergy_forecast.jpg")
 image = Image.open("allergy_forecast.jpg")
 
+template = """<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="styles.css">
+    <title>Daily Austin Forecast</title>
+</head>
+<body>
+
+<h1>Forecast</h1>
+<div>
+    FORECAST_INFORMATION
+</div>
+<img src="allergy_forecast.jpg"></img>
+
+</body>
+</html>"""
+template = template.replace("FORECAST_INFORMATION", str(weatherText))
+
+with open("forecast.html", "w") as file:
+    file.write(template)
